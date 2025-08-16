3b3b51ceb8db1bdb7c73f00ffaa53b3f582ea6e0
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-08-15 19:20:54 -0700

    Use MOD_LALT instead of KC_LALT when Tab held

--- perf60/keymaps/default/keymap.c
+++ perf60/keymaps/default/keymap.c
@@ -4,13 +4,14 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 /* Base (Layer 0)
  * ,-----------------------------------------.      ,-----------------------------------------.
  * |Raise |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  | F11  |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |  =   |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |Ctl-B |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * |  `   |   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  |  -   |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
@@ -19,10 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [0] = LAYOUT(
   TO(1),   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F11,
-  MT(KC_LALT, KC_TAB),    // Tab tap, Alt hold
+  MT(MOD_LALT, KC_TAB),    // Tab tap, Alt hold
            KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
-  C(KC_B), // Ctrl+B tap, Ctrl hold
-           KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  C(KC_B), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     LT(1, KC_ESC),          // Escape tap, Layer 1 hold
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * |      |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |C-S-V |  =   |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |      | Left | Down |  Up  |Right |   ;  |  "   |
+ * |      |   A  |   S  |   D  |   F  |   G  |      | Left | Down |  Up  |Right |   ;  |  "   |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * |  `   |   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   (  |   )  |   /  |  -   |
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    C(S(KC_V)),
                                                                                                      KC_EQL,
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_QUOT,
+  _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_QUOT,
   KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    S(KC_9), S(KC_0), KC_SLSH, KC_MINS,
   TO(0),   XXXXXXX, XXXXXXX, XXXXXXX, _______, S(KC_ENTER),
                                                         _______, _______, S(KC_9), S(KC_0), KC_BSLS, KC_LGUI
